0e2a9d1ea141c09ca8558cdd1869f6ca611d6840
B-Michaelx <159101600+B-Michaelx@users.noreply.github.com>
2024-04-01 09:29:51 -0600

    Update ejercicio.php

--- ejercicio.php
+++ ejercicio.php
@@ -1,3 +1,52 @@
 <?php
-    echo"Hola Mundo"
+echo"Hola Mundo"
+//ejercicio 1
+//comentamos con // o #
+ //declaramos variables con $ 
+$numero1 = 5;
+$numero2 = 10;
+$suma = $numero1 + $numero2;
+echo "la suma de $numero1 y $numero2 es $suma";
+
+//ejercicio 2
+$numero = -5;
+if ($numero > 0) {
+echo "el numero es positivo";
+} elseif ($numero < 0) {
+    echo "el numero es negativo";
+} else {
+    echo "el numero es cero";
+}
+
+//ejercicio 3
+for ($i = 1; $i <= 10; $i++){
+    echo $i . "<br>";
+}
+
+//ejercicio 4
+//crear un index.php
+<form action="saludo.php" method="post">
+    Nombre: <input type="text" name="nombre">
+    <input type="submit">
+</form>
+    
+//creamos un saludo.php
+$nombre = $_POST['nombre'];
+echo "Hola, $nombre!";
+
+//ejercicio 1 definitivo
+//crear un index.php
+//crear formulario
+//poner como titulo conversor de temperatura
+//poner un p para instrucciones en el index
+<form action="saludo.php" method="post">
+    Ingrese la temperatura: <input type="text" name="nombre">
+    <input type="submit">
+</form>
+
+
+
+
+
+
 ?>
